efacbc599a63b2ac7e9c911029dd5de7ff9419bb
Hartman <imh29@drexel.edu>
2016-11-30 18:48:27 -0500

    updated setup file

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@ from distutils.core import setup
 import py2exe
 
 setup(
+    name='QubeSubmit',
+    version='0.1.0',
+    description='Qube Submission GUI',
+    author='Ian Hartman',
     options={
         "py2exe": {
             "dll_excludes": ["MSVCP90.dll"]
